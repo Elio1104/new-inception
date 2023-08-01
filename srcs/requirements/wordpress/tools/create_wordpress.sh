d37a711211846c23427af98542aff00668cc5f72
alondot <alondot@gmail.com>
2023-08-01 23:48:06 +0200

    update wp

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -9,7 +9,6 @@ else
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
-	rm -rf wordpress
 
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
