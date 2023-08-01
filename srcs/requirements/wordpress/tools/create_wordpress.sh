b97571a39c22536067a06ae7028e5dfe2560023e
alondot <alondot@gmail.com>
2023-08-01 23:44:22 +0200

    fin de wordpress

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,20 +1,22 @@
-#!bin/bash
+#!/bin/sh
 
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root \
-						--dbname=$MYSQL_DATABASE \
-						--dbuser=$MYSQL_USER \
-						--dbpass=$MYSQL_PASSWORD \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
 
-    wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-    
-    wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
-    
-    wp theme install hestia –activate
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+
 fi
 
-if [ ! -d /run/php ]; then
-    mkdir ./run/php
-fi
-exec /usr/sbin/php-fpm7.3 -F
+exec "$@"
