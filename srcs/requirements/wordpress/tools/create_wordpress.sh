c9d703e817f67af4e467e972c02ad99521ea3a68
alondot <alondot@gmail.com>
2023-08-02 00:47:33 +0200

    wp update

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -11,6 +11,7 @@ wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_U
     
 wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 
-cp ./config.php ./wp-config.php
+rm ./wp-config.php
+mv ./config.php ./wp-config.php
 
 exec "$@"
