5ed655c89232601e3a800b8b50bf2dbfd8a1b583
alondot <alondot@gmail.com>
2023-08-01 23:28:26 +0200

    ajout theme

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -10,6 +10,8 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
     
     wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+    
+    wp theme install hestia –activate
 fi
 
 if [ ! -d /run/php ]; then
