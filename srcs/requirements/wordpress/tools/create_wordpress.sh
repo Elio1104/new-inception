b551d525ed23b622ff87062ac8663fe1364b9735
alondot <alondot@gmail.com>
2023-08-01 23:27:43 +0200

    test

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,7 +1,5 @@
 #!bin/bash
 
-sleep 10
-
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create	--allow-root \
 						--dbname=$MYSQL_DATABASE \
@@ -17,4 +15,4 @@ fi
 if [ ! -d /run/php ]; then
     mkdir ./run/php
 fi
-/usr/sbin/php-fpm7.3 -F
+exec /usr/sbin/php-fpm7.3 -F
