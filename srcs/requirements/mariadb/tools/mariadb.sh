227855f737a1d8e87178965055b7194437c6ac34
alondot <alondot@gmail.com>
2023-08-01 21:35:55 +0200

    echo test

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,19 +8,19 @@ export MYSQL_ROOT_PASSWORD=root1234
 service mysql start;
 
 echo "1"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 echo "2"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 echo "3"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 echo "4"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 echo "5"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
 mysql -e "FLUSH PRIVILEGES;"
 echo "6"
-service status mysql
+mysql -e "SHOW GLOBAL STATUS;"
