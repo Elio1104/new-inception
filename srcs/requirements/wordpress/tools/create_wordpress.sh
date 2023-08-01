2a23bd38eb61ada0653a2ce738f4c9c2ac312dc2
alondot <alondot@gmail.com>
2023-08-02 01:38:35 +0200

    je comprends plus rien

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -2,13 +2,14 @@
 
 wp core download    --allow-root
 echo "1"
-wp core config    --dbname=$MYSQL_DATABASE \
+wp core config		--dbname=$MYSQL_DATABASE \
 					--dbuser=$MYSQL_USER \
 					--dbpass=$MYSQL_PASSWORD \
 					--dbhost="mariadb":"3306" \
                     --dbprefix='wp_' \
                     --allow-root
 echo "2"
+ls
 wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
 echo "3"
 wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
