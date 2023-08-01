0ca67c94eaa6750ec93e324bb65091403e42cdeb
alondot <alondot@gmail.com>
2023-08-02 00:30:32 +0200

    update wp

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/wp-config-sample.php .
-    rm -rf ./wordpress/
-
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-
-fi
-
 wp core download    --allow-root
 wp core config      --dbname=$MYSQL_DATABASE \
 					--dbuser=$MYSQL_USER \
