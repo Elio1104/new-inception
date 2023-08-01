9c00e5962e80964120cbdd203a51383ccad5ca12
alondot <alondot@gmail.com>
2023-08-02 01:00:02 +0200

    test wp

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -2,7 +2,7 @@
 
 wp core download    --allow-root
 echo "1"
-wp core config      --dbname=$MYSQL_DATABASE \
+wp config create    --dbname=$MYSQL_DATABASE \
 					--dbuser=$MYSQL_USER \
 					--dbpass=$MYSQL_PASSWORD \
 					--dbhost=mariadb:3306 --path='/var/www/wordpress' \
