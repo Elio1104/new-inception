d50cffc38bd2e42148960194b0103fa0185cf950
alondot <alondot@gmail.com>
2023-08-01 16:16:24 +0200

    modif env

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-service mysql start 
+export MYSQL_DATABASE=inception
+export MYSQL_USER=alondot
+export MYSQL_PASSWORD=test1234
+export MYSQL_ROOT_PASSWORD=root1234
+
+service mysql start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
