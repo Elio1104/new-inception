6adfbdf1dca47c30611e55550273f5a0f50597e9
alondot <alondot@gmail.com>
2023-08-02 12:08:35 +0200

    update WP

--- srcs/requirements/wordpress/conf/wp-conf.sh
+++ srcs/requirements/wordpress/conf/wp-conf.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-wp core download    --allow-root
-wp config create	--dbname="$MYSQL_DATABASE"\
-					--dbuser="$MYSQL_USER" \
-					--dbpass="$MYSQL_PASSWORD" \
-					--dbhost="$DB_HOST":"3306" \
-                    --dbprefix='wp_' \
-                    --allow-root
-wp core install     --url="$DOMAIN_NAME" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
-wp user create      --allow-root --role='author' $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS
-wp theme install inspiro --activate --allow-root
-wp option update comment_moderation 0
+if [ -f ./wp-config-sample.php ]
+then
+	echo "wordpress already downloaded"
+else
+
+	wp core download    --allow-root
+	wp config create	--dbname="$MYSQL_DATABASE"\
+						--dbuser="$MYSQL_USER" \
+						--dbpass="$MYSQL_PASSWORD" \
+						--dbhost="$DB_HOST":"3306" \
+						--dbprefix='wp_' \
+						--allow-root
+	wp core install     --url="$DOMAIN_NAME" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
+	wp user create      --allow-root --role='author' $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS
+	wp theme install inspiro --activate --allow-root
+	wp option update comment_moderation 0 --allow-root
+
+fi
 exec /usr/sbin/php-fpm7.3 -F
