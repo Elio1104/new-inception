b3b96dc1f505afb78d12cff3304b2ab8f0312b17
alondot <alondot@gmail.com>
2023-08-02 03:03:29 +0200

    wp test

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -9,4 +9,4 @@ wp core config		--dbname="$MYSQL_DATABASE"\
                     --allow-root
 wp core install     --url="$DOMAIN_NAME" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
 wp user create      --allow-root --role='author' $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS
-exec "$@"
+exec /usr/sbin/php-fpm7.3 -F
