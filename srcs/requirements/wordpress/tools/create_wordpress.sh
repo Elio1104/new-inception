fe3f6d329522f4c21791304f9a7185f23ac9af7d
alondot <alondot@gmail.com>
2023-08-02 03:18:10 +0200

    test2001

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-wp core download --allow-root
-wp core config --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$DB_HOST":"3306" --dbprefix='wp_' --allow-root
-wp core install --url="$DOMAIN_NAME" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
-wp user create $USER1_LOGIN $USER1_PASS --role='author' --user_pass="$USER1_PASS" --allow-root
+
+wp core download    --allow-root
+wp core config		--dbname="$MYSQL_DATABASE"\
+					--dbuser="$MYSQL_USER" \
+					--dbpass="$MYSQL_PASSWORD" \
+					--dbhost="$DB_HOST":"3306" \
+                    --dbprefix='wp_' \
+                    --allow-root
+wp core install     --url="$DOMAIN_NAME" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root
+wp user create      --allow-root --role='author' $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS
 exec /usr/sbin/php-fpm7.3 -F
